40f87e44e41754d2a5ffa5ee96010336b5e11059
Rishita Panchal <rishitapanchal2707@gmail.com>
2022-07-11 16:09:57 +0530

    ADD: Signup branch

---
+++ app/src/main/java/Admin/login/branch/login_demo_activity.kt
@@ -0,0 +1,12 @@
+package Admin.login.branch
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.miniproject.R
+
+class login_demo_activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_demo)
+    }
+}
